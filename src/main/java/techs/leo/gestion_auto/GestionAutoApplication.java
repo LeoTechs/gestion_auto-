409af62da9b37f322d40b7bdb1a536a1d1f69663
Leotechs <techsleo@gmail.com>
2025-01-13 11:38:25 +0100

    Initialisation du projet

---
+++ src/main/java/techs/leo/gestion_auto/GestionAutoApplication.java
@@ -0,0 +1,13 @@
+package techs.leo.gestion_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techs/leo/gestion_auto/GestionAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package techs.leo.gestion_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
